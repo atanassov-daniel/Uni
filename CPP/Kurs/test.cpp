7275837c59306e8fe1a9cf289954e77c0ff6b5ed
atanassov-daniel <atanassov.daniel@gmail.com>
2023-10-04 12:30:45 +0300

    Hello world example

--- CPP/Kurs/test.cpp
+++ CPP/Kurs/test.cpp
@@ -1,1 +1,15 @@
-sdd
+// C++ program to display "Hello World"
+
+// Header file for input output functions
+#include <iostream>
+using namespace std;
+
+// Main() function: where the execution of
+// program begins
+int main()
+{
+	// Prints hello world
+	cout << "Hello World" << endl;
+
+	return 0;
+}
